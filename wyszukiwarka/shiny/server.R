3b342135399f9b403f3dff7065b9982edec8144d
Marta Czarnocka-Cieciura <martaczc@interia.pl>
2015-09-26 15:36:49 +0200

    progress message

--- wyszukiwarka/shiny/server.R
+++ wyszukiwarka/shiny/server.R
@@ -8,18 +8,24 @@ shinyServer(function(input, output) {
     input$slowo
   })
   
+  # pokazuje progress przy obliczeniach
+                 
   output$tekst <- renderUI({
-    cat(wartosc(), file = stderr()) # diagnostyka: wypisywanie wartosci w konsoli
-    getBorders(wartosc())
+    #cat(wartosc(), file = stderr()) # diagnostyka: wypisywanie wartosci w konsoli
+    withProgress(message = 'Obliczam,',
+                 detail = 'Może chwilę potrwać...', value = 0, {
+                   getBorders(wartosc())
+                 })
   })
   
   output$testPlot1 <- renderPlot({
-    cat(wartosc(), file = stderr()) # diagnostyka: wypisywanie wartosci w konsoli
+    #cat(wartosc(), file = stderr()) # diagnostyka: wypisywanie wartosci w konsoli
     getSpeakerCounts(wartosc())
   })
   
   output$testPlot2 <- renderPlot({
-    cat(wartosc(), file = stderr()) # diagnostyka: wypisywanie wartosci w konsoli
+    #cat(wartosc(), file = stderr()) # diagnostyka: wypisywanie wartosci w konsoli
     getDateCounts(wartosc())
   })
+  
 })

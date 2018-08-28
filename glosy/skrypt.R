61ce95c295d0d09498ccbc3e259992417bf8f687
Fredrik Wollsén <fredrik@neam.se>
2018-08-28 23:20:52 +0300

    Clarified variable names

--- glosy/skrypt.R
+++ glosy/skrypt.R
@@ -8,8 +8,7 @@ library(parallel)
 
 numCores <- detectCores() # get the number of cores available
 
-kol <- brewer.pal(9,"Set1")
-kol2 <- c(PO = "orange3", PiS = "blue4", RP = "gold3", PSL="green4", SLD="red3", SP="blue1",
+partyColors <- c(PO = "orange3", PiS = "blue4", RP = "gold3", PSL="green4", SLD="red3", SP="blue1",
           `niez.` = "grey", ID="gold4", TR="gold3", KPSP="blue2", BiG="orange2",
           ZP="blue2", BC ="blue2" )
 scores <- c(`Nieobecny` = 0, `Przeciw` = -2, `Wstrzymał się` = -1, `Za` = 2)
@@ -18,40 +17,42 @@ scores <- c(`Nieobecny` = 0, `Przeciw` = -2, `Wstrzymał się` = -1, `Za` = 2)
 
 load("all_votes.rda")
 
+# the data column "club" stands for which party/parties the member was a member of
+# translating here to "party" for clarity
+all_votes$party <- all_votes$club
 
-# wzor = pattern
-wzor <- "o ochronie zwierząt" # about animal protection
-wzor <- "szkolnict" # school
-wzor <- ""
+# pattern = pattern
+pattern <- "o ochronie zwierząt" # about animal protection
+pattern <- "szkolnict" # school
+pattern <- ""
 
+# debug
+# head(grep(unique(all_votes$topic_voting), pattern = "szkolnict", value = TRUE))
 
-head(grep(unique(all_votes$topic_voting), pattern = "szkolnict", value = TRUE))
+votingTopicsThatMatchesPattern <- grep(unique(all_votes$topic_voting), pattern = pattern, value = TRUE)
 
-# tutuly glos = voting topics
-tytulyGlos <- grep(unique(all_votes$topic_voting), pattern = wzor, value = TRUE)
+selectionOfVotes <- all_votes %>%
+  filter(topic_voting %in% votingTopicsThatMatchesPattern)
 
-selVotes <- all_votes %>%
-  filter(topic_voting %in% tytulyGlos)
-
-tabi2 <- table(selVotes$club,selVotes$vote)[,c(4,1,3,2)]
-mosaicplot(tabi2, off = c(0,0), border="white", 
+partiesAndTheirVotes <- table(selectionOfVotes$party,selectionOfVotes$vote)[,c(4,1,3,2)]
+mosaicplot(partiesAndTheirVotes, off = c(0,0), border="white", 
            color=c("green3", "grey", "red4", "red2"), las=2,
            main="")
 
 
 
-tabi <- table(selVotes$surname_name, selVotes$club)
-clubs <- apply(tabi, 1, function(x) paste(colnames(tabi)[x>0], collapse=","))
-clubs2 <- apply(tabi, 1, function(x) paste(colnames(tabi)[which.max(x)], collapse=","))
+membersAndTheirParties <- table(selectionOfVotes$surname_name, selectionOfVotes$party)
+partiesIncludingHistory <- apply(membersAndTheirParties, 1, function(x) paste(colnames(membersAndTheirParties)[x>0], collapse=","))
+partiesOnlyLatest <- apply(membersAndTheirParties, 1, function(x) paste(colnames(membersAndTheirParties)[which.max(x)], collapse=","))
 
-selVotes$vote <- scores[as.character(selVotes$vote)]
+selectionOfVotes$vote <- scores[as.character(selectionOfVotes$vote)]
 
-tVotes <- spread(selVotes[,c(1,3,4)], key = id_voting, value = vote)
-rownames(tVotes) <- paste(tVotes[,1], " - ", clubs[as.character(tVotes[,1])], sep="")
+tVotes <- spread(selectionOfVotes[,c(1,3,4)], key = id_voting, value = vote)
+rownames(tVotes) <- paste(tVotes[,1], " - ", partiesIncludingHistory[as.character(tVotes[,1])], sep="")
 tVotes <- tVotes[,-1]
 
 # tylko Ci w sejmie na ponad 90% glosowan = only those in the Seym over 90% of the votes
-cVotes <- clubs2[rowMeans(is.na(tVotes)) < 0.1]
+cVotes <- partiesOnlyLatest[rowMeans(is.na(tVotes)) < 0.1]
 tVotes <- tVotes[rowMeans(is.na(tVotes)) < 0.1,]
 
 
@@ -64,28 +65,28 @@ hc = as.hclust(ag)
 par(mar=c(1,1,2,1), xpd=NA)
 
 plot(as.phylo(hc), type = "fan", cex = 0.4,
-     tip.color = kol2[cVotes],
-     main=wzor,
+     tip.color = partyColors[cVotes],
+     main=pattern,
      rotate.tree=-85)
 
 plot(as.phylo(hc), type = "unrooted", cex = 0.4,
-     tip.color = kol2[cVotes],
-     main=wzor,
+     tip.color = partyColors[cVotes],
+     main=pattern,
      rotate.tree=-85)
 
 plot(as.phylo(hc), type = "radial", cex = 0.4,
-     tip.color = kol2[cVotes],
-     main=wzor,
+     tip.color = partyColors[cVotes],
+     main=pattern,
      rotate.tree=-85)
 
 plot(as.phylo(hc), type = "phylogram", cex = 0.4,
-     tip.color = kol2[cVotes],
-     main=wzor,
+     tip.color = partyColors[cVotes],
+     main=pattern,
      rotate.tree=-85)
 
 plot(as.phylo(hc), type = "cladogram", cex = 0.4,
-     tip.color = kol2[cVotes],
-     main=wzor,
+     tip.color = partyColors[cVotes],
+     main=pattern,
      rotate.tree=-85)
 
 
@@ -111,41 +112,40 @@ ustawy3 <- names(which(table(ustawy3) > 5))
 
 
 
+uniqueMemberNames <- levels(factor(selectionOfVotes$surname_name))
 
-selPos <- levels(factor(selVotes$surname_name))
+membersAndTheirParties <- table(selectionOfVotes$surname_name, selectionOfVotes$party)
+partiesIncludingHistory <- apply(membersAndTheirParties, 1, function(x) paste(colnames(membersAndTheirParties)[x>0], collapse=","))
 
-tabi <- table(selVotes$surname_name, selVotes$club)
-clubs <- apply(tabi, 1, function(x) paste(colnames(tabi)[x>0], collapse=","))
+partiesOnlyLatest <- apply(membersAndTheirParties, 1, function(x) paste(colnames(membersAndTheirParties)[which.max(x)], collapse=","))
 
-clubs2 <- apply(tabi, 1, function(x) paste(colnames(tabi)[which.max(x)], collapse=","))
-
-distMat <- matrix(NA, length(selPos),  length(selPos))
-colnames(distMat) <- paste0(selPos, " (", clubs, ")")
-rownames(distMat) <- paste0(selPos, " (", clubs, ")")
+distMat <- matrix(NA, length(uniqueMemberNames),  length(uniqueMemberNames))
+colnames(distMat) <- paste0(uniqueMemberNames, " (", partiesIncludingHistory, ")")
+rownames(distMat) <- paste0(uniqueMemberNames, " (", partiesIncludingHistory, ")")
 
 recalculateDistMat <- TRUE
 if (recalculateDistMat) {
   system.time({
     
-    iMax <- (length(selPos)-1)
-    jMax <- length(selPos)
+    iMax <- (length(uniqueMemberNames)-1)
+    jMax <- length(uniqueMemberNames)
 
     message("Pre-calculating lists of personal votes")
-    results_selVotes <- mclapply(1:jMax,
+    results_selectionOfVotes <- mclapply(1:jMax,
                                  FUN=function(i) {
-                                   selVotesI <- selVotes %>%
-                                     filter(surname_name %in% selPos[i])
+                                   selectionOfVotesI <- selectionOfVotes %>%
+                                     filter(surname_name %in% uniqueMemberNames[i])
                                  },
                                  mc.cores = numCores)
     
     message("Summarizing intersections between member votes")
     for (i in 1:iMax) {
-      cat("\n",i," ", selPos[i]," ")
+      cat("\n",i," ", uniqueMemberNames[i]," ")
       for (j in i:jMax) {
-        selVotesI <- results_selVotes[[i]]
-        selVotesJ <- results_selVotes[[j]]
+        selectionOfVotesI <- results_selectionOfVotes[[i]]
+        selectionOfVotesJ <- results_selectionOfVotes[[j]]
 
-        selIJ <- merge(selVotesI[,c(1,2,3,4)], selVotesJ[,c(1,2,3,4)], by="id_voting")
+        selIJ <- merge(selectionOfVotesI[,c(1,2,3,4)], selectionOfVotesJ[,c(1,2,3,4)], by="id_voting")
         
         distMat[i,j] = mean(selIJ$vote.x == selIJ$vote.y)
         distMat[j,i] = mean(selIJ$vote.x == selIJ$vote.y)
@@ -161,22 +161,22 @@ if (recalculateDistMat) {
 
 rem <- which(rowMeans(is.na(distMat)) > 0.01)
 distMatR <- distMat[-rem, -rem]
-rownames(distMatR) <- selPos[-rem]
-colnames(distMatR) <- paste(selPos[-rem], clubs2[-rem])
+rownames(distMatR) <- uniqueMemberNames[-rem]
+colnames(distMatR) <- paste(uniqueMemberNames[-rem], partiesOnlyLatest[-rem])
 
 library(MASS)
 
 space <- isoMDS(as.dist(1.001-distMatR), k=2)
-df <- data.frame(space$points, clubs=clubs2[-rem], name=selPos[-rem])
+df <- data.frame(space$points, parties=partiesOnlyLatest[-rem], name=uniqueMemberNames[-rem])
 
 library(ggplot2)
 
-ggplot(df, aes(X1, X2, color=clubs, label=name)) +
+ggplot(df, aes(X1, X2, color=parties, label=name)) +
   geom_text(size=4) +
   theme_bw() + scale_shape_manual(values=LETTERS)
 
-
-distMatR["Napieralski Grzegorz",]
+# debug
+# distMatR["Napieralski Grzegorz",]
 
 
 library(cluster)
@@ -187,14 +187,14 @@ library(ggdendro)
 library(ape)
 library(RColorBrewer)
 
-kol <- brewer.pal(9,"Set1")
+colors <- brewer.pal(9,"Set1")
 
 hc = as.hclust(ag)
 
 par(mar=c(0,0,2,0))
 
 plot(as.phylo(hc), type = "fan", cex = 0.4,
-     tip.color = kol[as.numeric(factor(clubs2[-rem]))],
-     main=wzor)
+     tip.color = colors[as.numeric(factor(partiesOnlyLatest[-rem]))],
+     main=pattern)
 
 
